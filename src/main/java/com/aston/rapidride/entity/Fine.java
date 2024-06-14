bd02614463cb978ea20ddf7a1b8d1f6b94040ce1
Evgenii Anchugov <bergy@yandex.ru>
2024-06-14 21:52:52 +0300

    added fine

--- src/main/java/com/aston/rapidride/entity/Fine.java
+++ src/main/java/com/aston/rapidride/entity/Fine.java
@@ -1,12 +1,28 @@
 package com.aston.rapidride.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
+@Getter
+@Setter
 @Entity
 @NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class Fine {
     @Id
     Long id;
+    @NonNull
+    Long carId;
+    Long userId;
+    @NonNull
+    LocalDate date;
+    @Column(name = "summ")
+    BigDecimal sum;
+    @NonNull
+    String registration_number;
+    @NonNull
+    Long payment_id;
 }
