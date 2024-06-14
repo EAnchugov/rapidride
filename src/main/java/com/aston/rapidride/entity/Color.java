2d4f8882fc09afa20df62e0df9a9ee630a0e4bcb
AzimovRuslan <55226276+AzimovRuslan@users.noreply.github.com>
2024-06-14 20:38:34 +0300

    has been created Model, Color, EngineType and Brand entities

--- src/main/java/com/aston/rapidride/entity/Color.java
+++ src/main/java/com/aston/rapidride/entity/Color.java
@@ -1,15 +1,18 @@
 package com.aston.rapidride.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "brands")
-public class Brand {
+@Table(name = "colors")
+public class Color {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
