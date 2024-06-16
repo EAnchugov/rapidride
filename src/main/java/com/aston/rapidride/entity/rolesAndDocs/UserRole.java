b47d1a081f5a2089a840700b5eb2325474acc659
Sanek12382 <aleksondrf@bk.ru>
2024-06-16 18:46:21 +0300

    add entities user_documents, users_roles, roles, document_type users_roles не сделана так как должна представляться отношением между role и user user_documents не готов так как userID требует user для постоения отношения

--- src/main/java/com/aston/rapidride/entity/rolesAndDocs/UserRole.java
+++
@@ -1,9 +0,0 @@
-package com.aston.rapidride.entity.rolesAndDocs;
-
-
-
-
-public class UserRole {
-
-    //вместо этой энтити надо использовать отнощение hibernate(https://habr.com/ru/articles/542328/)
-}
