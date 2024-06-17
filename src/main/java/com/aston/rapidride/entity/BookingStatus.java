28dc0d2bbdd0af62da4f6517aa4d60a5c90b40e1
Sergeev Maxim <ser.maxix@yandex.ru>
2024-06-17 19:27:23 +0300

    add booking status

---
+++ src/main/java/com/aston/rapidride/entity/BookingStatus.java
@@ -0,0 +1,29 @@
+package com.aston.rapidride.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "booking_statuses")
+public class BookingStatus {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "status")
+    protected Set<Booking> bookings;
+}
