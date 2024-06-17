6e60e94569759f9230d9bbfd71143de15aa53fe4
Sanek12382 <aleksondrf@bk.ru>
2024-06-17 09:30:57 +0300

    add entities user_documents, users_roles, roles, document_type users_roles не сделана так как должна представляться отношением между role и user user_documents не готов так как userID требует user для постоения отношения

--- src/main/java/com/aston/rapidride/entity/rolesAndDocs/UserDocument.java
+++ src/main/java/com/aston/rapidride/entity/rolesAndDocs/UserDocument.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 
 @Entity
 @Getter
@@ -19,9 +21,14 @@ public class UserDocument {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     //тербуется сам user чтобы домавить отношение
+    @Column(name = "user_id", nullable = false)
+    @NotBlank(message = "user_id can't be empty")
     Long userID;
-
+    @Column(name = "first_name", nullable = false)
+    @NotBlank(message = "first_name can't be empty")
     private String firstName;
+    @Column(name = "last_name", nullable = false)
+    @NotBlank(message = "last_name can't be empty")
     private String lastName;
 
     @ManyToOne
