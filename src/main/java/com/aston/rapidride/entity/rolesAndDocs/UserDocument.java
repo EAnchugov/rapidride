9b1860e9e35a948113586702999d1e35eef4b525
Sanek12382 <aleksondrf@bk.ru>
2024-06-18 13:03:51 +0300

    updated userdocument with relation to user

--- src/main/java/com/aston/rapidride/entity/rolesAndDocs/UserDocument.java
+++ src/main/java/com/aston/rapidride/entity/rolesAndDocs/UserDocument.java
@@ -20,15 +20,17 @@ public class UserDocument {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    //тербуется сам user чтобы домавить отношение
+
     @ManyToOne
     @JoinColumn(name = "id")
     @Column(name = "user_id", nullable = false)
     @NotBlank(message = "user_id can't be empty")
     Long userID;
+
     @Column(name = "first_name", nullable = false)
     @NotBlank(message = "first_name can't be empty")
     private String firstName;
+
     @Column(name = "last_name", nullable = false)
     @NotBlank(message = "last_name can't be empty")
     private String lastName;
