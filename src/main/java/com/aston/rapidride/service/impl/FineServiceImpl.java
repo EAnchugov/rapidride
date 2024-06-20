febdd46dd365ef196ca1e0aa6db151a08f03ca9a
Evgenii Anchugov <bergy@yandex.ru>
2024-06-20 13:32:34 +0300

    added transaction

--- src/main/java/com/aston/rapidride/service/impl/FineServiceImpl.java
+++ src/main/java/com/aston/rapidride/service/impl/FineServiceImpl.java
@@ -5,6 +5,7 @@ import com.aston.rapidride.entity.Fine;
 import com.aston.rapidride.entity.User;
 import com.aston.rapidride.repository.FineRepository;
 import com.aston.rapidride.service.FineService;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,51 +19,62 @@ public class FineServiceImpl implements FineService {
     private final FineRepository repository;
 
     @Override
+    @Transactional
     public Fine getById(Long id) {
-        return repository.findById(id).orElseThrow();
+        return repository.findById(id).orElse(new Fine());
     }
 
     @Override
+    @Transactional
+
     public void createFine(Fine fine) {
         repository.save(fine);
     }
 
     @Override
+    @Transactional
     public void updateFine(Fine fine) {
         repository.save(fine);
     }
 
     @Override
+    @Transactional
     public List<Fine> getAllFine() {
         return repository.findAll();
     }
 
     @Override
+    @Transactional
     public List<Fine> getAllFineByUser(User user) {
         return repository.findAllByUserId(user.getId());
     }
 
     @Override
+    @Transactional
     public List<Fine> getAllFineByCar(Car car) {
         return repository.findAllByCar(car);
     }
 
     @Override
+    @Transactional
     public Fine getFineByUserAndCar(User user, Car car) {
         return repository.findAllByCarIdAndUserId(car.getId(), user.getId());
     }
 
     @Override
+    @Transactional
     public List<Fine> getFinesByRegistrationNumber(Long registrationNumber) {
         return repository.findAllByRegistrationNumber(registrationNumber);
     }
 
     @Override
+    @Transactional
     public List<Fine> getAllFinesBySum(BigDecimal sum) {
         return repository.findAllBySum(sum);
     }
 
     @Override
+    @Transactional
     public List<Fine> getAllByDate(LocalDate date) {
         return repository.findAllByDate(date);
     }
