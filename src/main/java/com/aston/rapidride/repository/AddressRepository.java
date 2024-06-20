86d3fd48d7c93a34a9dca3674960de9c6813cb68
Murad Raimjanov <raimzhanov.muradil@gmail.com>
2024-06-20 16:08:21 +0300

    delete address

--- src/main/java/com/aston/rapidride/repository/AddressRepository.java
+++ src/main/java/com/aston/rapidride/repository/AddressRepository.java
@@ -1,11 +1,10 @@
-package com.aston.rapidride.repository.user;
+package com.aston.rapidride.repository;
 
 import com.aston.rapidride.entity.Address;
-import com.aston.rapidride.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface AddressRepository extends JpaRepository<Address, Long> {
     
 }
