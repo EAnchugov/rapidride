580690a6c20467ece30877fd9fd9cdcaa3ca9462
Budkina Elizaveta <yurikoishihara2000@gmail.com>
2024-07-02 11:12:28 +0300

    Add payment service test

---
+++ src/test/java/com/aston/rapidride/controller/PaymentControllerTest.java
@@ -0,0 +1,40 @@
+package com.aston.rapidride.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentControllerTest {
+
+    @Test
+    void getPaymentsTest() {
+    }
+
+    @Test
+    void createPaymentTest() {
+    }
+
+    @Test
+    void updatePaymentTest() {
+    }
+
+    @Test
+    void getPaymentByIdTest() {
+    }
+
+    @Test
+    void deletePaymentByIdTest() {
+    }
+
+    @Test
+    void getPaymentSumTest() {
+    }
+
+    @Test
+    void getPaymentSenderTest() {
+    }
+
+    @Test
+    void getPaymentGetterTest() {
+    }
+}
